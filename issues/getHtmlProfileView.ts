4827dfee8d7da9dc6a9df9f86dfa91554c2b5b11
oplindstr <olli-pekkalindstrom@hotmail.com>
2022-07-05 12:59:36 +0300

    Issue #2: Sanitize all fields given in UserProfile

--- issues/getHtmlProfileView.ts
+++ issues/getHtmlProfileView.ts
@@ -8,10 +8,10 @@ interface UserProfile {
 export function getHtmlProfileView(userProfile: UserProfile): string {
   return `<div class="profile">
     <div class="name"><a href="${escapeHTMLAttribute(userProfile.url)}">${
-    userProfile.name
+      escapeHTMLAttribute(userProfile.name)
   }</a></div>
-  <div class="phone">${userProfile.phone}</div>
-  <div class="signature">${userProfile.signature.replace('\n', '<br>')}</div>
+  <div class="phone">${escapeHTMLAttribute(userProfile.phone)}</div>
+  <div class="signature">${escapeHTMLAttribute(userProfile.signature).replace('\n', '<br>')}</div>
     </div>`;
 }
 
